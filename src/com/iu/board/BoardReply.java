01c2168cdc1728072270c05f54f38940ce66cb13
Leeyus <43767943+Leeyus@users.noreply.github.com>
2018-10-26 11:20:44 +0900

    add_pager
    다 불리함

---
+++ src/com/iu/board/BoardReply.java
@@ -0,0 +1,13 @@
+package com.iu.board;
+
+public interface BoardReply {
+	
+	//reply
+	
+	public int reply(BoardReplyDTO boardReplyDTO) throws Exception;
+	
+	//replyUpdate
+	
+	public int replyUpdate(BoardReplyDTO boardReplyDTO) throws Exception;
+
+}
---
+++ src/com/iu/page/RowNumber.java
@@ -0,0 +1,31 @@
+package com.iu.page;
+
+public class RowNumber {
+	
+	private int startRow;
+	private int lasttRow;
+	private Search search;
+	
+	
+	public int getStartRow() {
+		return startRow;
+	}
+	public void setStartRow(int startRow) {
+		this.startRow = startRow;
+	}
+	public int getLasttRow() {
+		return lasttRow;
+	}
+	public void setLasttRow(int lasttRow) {
+		this.lasttRow = lasttRow;
+	}
+	public Search getSearch() {
+		return search;
+	}
+	public void setSearch(Search search) {
+		this.search = search;
+	}
+	
+	
+
+}
